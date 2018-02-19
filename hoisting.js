929bb466cfdc9fd3727af9974c7da4b72567e1b8
Jerry P <jpull@jpull.com>
2018-02-19 23:28:11 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,17 +1,15 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
-
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
@@ -25,8 +23,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-
     var name = "Kristin";
-        console.log(name);
+    console.log(name);
   }
 }
